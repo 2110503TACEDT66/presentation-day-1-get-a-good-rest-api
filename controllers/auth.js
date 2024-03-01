63d25bc337db2d6984535710b6461ef6c65612aa
flagrantii <tanuson679@gmail.com>
2024-03-02 00:58:23 +0700

    Add telephone schema

--- controllers/auth.js
+++ controllers/auth.js
@@ -3,15 +3,14 @@ const User = require('../models/User');
 
 exports.register = async (req, res, next) => {
     try{
-        const { name, email, password, role } = req.body;
+        const { name, telephone, email, password, role } = req.body;
         const user = await User.create({
             name,
+            telephone,
             email,
             password,
             role
         });
-        const token = user.getSignedJwtToken();
-        res.status(200).json({ success: true, token});
         sendTokenResponse(user, 200, res);
     } catch (error) {
         res.status(401).json({ success: false});
@@ -38,10 +37,6 @@ exports.login = async (req, res, next) => {
     if(!isMatch){
         return res.status(400).json({ success: false, error: 'Invalid credentials'});
     }
-
-
-    const token = user.getSignedJwtToken();
-    return res.status(200).json({ success: true, token});
     sendTokenResponse(user, 200, res);
     } catch (error) {
         res.status(401).json({ success: false, msg: 'Cannot convert email or password to string'});
